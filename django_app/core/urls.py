5ad78651ec2747be620615ed988a12935be28b88
Mandalaco-16 <simaopaulobernardo5@gmail.com>
2026-07-24 23:54:40 +0100

    Create urls.py

---
+++ django_app/core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve as media_serve
+
+urlpatterns = [
+    path("django-admin/", admin.site.urls),
+    path("", include("escola.urls")),
+    re_path(r"^media/(?P<path>.*)$", media_serve, {"document_root": settings.MEDIA_ROOT}),
+]
