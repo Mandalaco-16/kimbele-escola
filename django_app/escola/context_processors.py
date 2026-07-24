28b0482b91e9430c49af323f375e7bee0840cb9d
Mandalaco-16 <simaopaulobernardo5@gmail.com>
2026-07-25 00:40:32 +0100

    Add context processor for school data

---
+++ django_app/escola/context_processors.py
@@ -0,0 +1,5 @@
+def dados_escola(request):
+    return {
+        "SIGLA_ESCOLA": "Sistema de Comunicação de falta via SMS em tempo Real",
+        "NOME_ESCOLA": "Escola Primária N°013 Da Missão Católica de Kimbele",
+    }
